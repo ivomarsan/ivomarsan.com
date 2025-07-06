083e2c6cbc3b98ec0a15572356c8877a73d9236b
ivomarsan <ivomarlucio@gmail.com>
2025-07-06 16:45:51 -0300

    feat: implement PageLogo component and update layout structure in app.vue

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div
+    :class="$style.pageLayout"
+  >
+        <slot />
+  </div>
+</template>
+
+<style module lang="postcss">
+.pageLayout {
+  @apply bg-bg-primary relative flex min-h-dvh flex-col;
+
+  @media print {
+    background-color: #fff !important;
+  }
+
+  
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div :class="$style.index">
+    <PageLogo />
+  </div>
+</template>
+
+<style module lang="postcss">
+.index {
+  display: flex;
+  justify-content: center;
+  background-color: var(--black);
+  height: 100dvh;
+}
+</style>
