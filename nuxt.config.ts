59acd52cbbf4d5688e80ca555355334ebba1e8b5
ivomarsan <ivomarlucio@gmail.com>
2025-07-06 18:30:27 -0300

    feat: add @nuxthub/core dependency and configure in nuxt.config for enhanced module support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,24 @@
 import type { NuxtConfig } from 'nuxt/config';
 
+import { useHeadConfig } from "./config";
 
-// const { htmlAttrs, charset, viewport, title, meta, link } = useHeadConfig()
+const head = useHeadConfig()
 
 const nuxtConfig: NuxtConfig = {
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   
+  modules: [
+    '@nuxthub/core'
+  ],
+  
   // SEO Configuration
-  // app: {
-  //   head: {
-  //     htmlAttrs,
-  //     charset,
-  //     viewport,
-  //     title,
-  //     meta,
-  //     link
-  //   }
-  // },
+  app: {
+    head
+  },
 
   nitro: {
-    preset: 'bun',
     experimental: {
       openAPI: true
     }
