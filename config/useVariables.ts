28fabc5766fd8d8b2121ee78bb9df5d9d3ae9739
ivomarsan <ivomarlucio@gmail.com>
2025-07-06 17:58:32 -0300

    feat: add useVariables function for managing theme color and app name

---
+++ config/useVariables.ts
@@ -0,0 +1,12 @@
+export const useVariables = () => {
+  const themeColor = '#000000'
+
+  const appName = 'ivomarsan';
+
+  return {
+    $variables: {
+      themeColor,
+      appName
+    }
+  };
+}
