8789a262217aa5dff049ff3fec472482353b71ac
ivomarsan <ivomarlucio@gmail.com>
2025-07-07 00:05:17 -0300

    fix: add missing 'contain' property to pageLayout style for layout optimization

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,11 +13,10 @@
 <style module lang="postcss">
 .pageLayout {
   @apply bg-bg-primary relative flex min-h-dvh flex-col;
+  contain: layout style paint;
 
   @media print {
     background-color: #fff !important;
   }
-
-  
 }
 </style>
