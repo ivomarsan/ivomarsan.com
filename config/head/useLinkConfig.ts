50566b2870985dc9769617d29b8e8b286b643e51
ivomarsan <ivomarlucio@gmail.com>
2025-07-07 00:53:27 -0300

    fix: add dns-prefetch and preconnect links for Google Fonts to improve performance

--- config/head/useLinkConfig.ts
+++ config/head/useLinkConfig.ts
@@ -9,5 +9,22 @@ export const useLinkConfig = () => {
       type: 'image/x-icon',
       href: '/favicon.ico',
     },
+    {
+      rel: 'dns-prefetch',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'dns-prefetch',
+      href: 'https://fonts.gstatic.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com',
+    },
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.gstatic.com',
+      crossorigin: 'anonymous',
+    },
   ];
 };
